33f83fb5a615267dced9370a85144216e0fe768e
Arkadiusz Kisiel <arkadiusz.kisiel@tomtom.com>
2017-01-18 14:11:16 +0100

    Added interpretation of all found kinds of SR definition in old documentation.

--- fatcat/oldcat/urls.py
+++ fatcat/oldcat/urls.py
@@ -4,7 +4,30 @@ from . import views
 
 urlpatterns = [
     url(r'^testcase/$', views.list_cases, name='list_cases'),
-    url(r'^requirement/(?P<sysReq>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
     url(r'^filename/(?P<filename>[\w\-]+).csv/$', views.list_filename, name='filename'),
     url(r'^testcase/(?P<testCaseId>[0-9]+)/$', views.test_case, name='test_case'),
+
+    url(r'^requirement/SR: (?P<sysReq>[\w\-]+)US: (?P<us>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/SR: (?P<sysReq>[\w\-]+)US: (?P<us_endspace>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/SR:(?P<sysReq>[\w\-]+)US:(?P<us_nospace>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/SR: (?P<sysReq>[\w\-]+) / US: (?P<us_slashed>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/SR: <EMPTY>US: (?P<us_EMPTY>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/S-R: (?P<sysReq>[\w\-]+)  U-S: (?P<us_dashed>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/SR (?P<sr_short>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+
+
+
+    url(r'^requirement/User Story: (?P<us>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/US (?P<us_short>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/US: (?P<us_short_dotted>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/US:(?P<us_nospace_inverse>[\w\-]+)SR:(?P<sysReq>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/US: (?P<us_joined_inverse>[\w\-]+)SR: (?P<sysReq>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/US (?P<us_joined_inverse_nofirstdots>[\w\-]+)SR: (?P<sysReq>[\w\-]+)/$', views.list_sysReq,
+        name='sysReq'),
+
+    url(r'^requirement/Bug Fix- (?P<bugfix>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/(?P<sysReq>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/(?P<sysReq>[\w\-]+) / (?P<us_noletters>[\w\-]+)/$', views.list_sysReq, name='sysReq'),
+    url(r'^requirement/N/A/$', views.list_sysReq, name='sysReq'),
+
 ]
--- fatcat/oldcat/views.py
+++ fatcat/oldcat/views.py
@@ -5,8 +5,50 @@ from django.template.response import TemplateResponse
 from .models import OldCase
 from django.shortcuts import render
 
-def list_sysReq(request, sysReq):
+def list_sysReq(request, sysReq=None, us=None, us_nospace=None, us_slashed=None,
+                us_dashed=None, us_nospace_inverse=None, bugfix=None, bug=None, us_short=None, us_EMPTY=None,
+                us_joined_inverse=None, us_joined_inverse_nofirstdots=None, us_noletters=None, us_short_dotted=None, sr_short=None):
+    check = False
+    if sysReq:
+        if us:
+            sysReq = 'SR: ' + sysReq + '\nUS: ' + us
+            check = True
+        if us_nospace:
+            sysReq = 'SR:' + sysReq + '\nUS:' + us_nospace
+        if us_slashed:
+            sysReq = 'SR: ' + sysReq + ' / US: ' + us_slashed
+        if us_dashed:
+            sysReq = 'S-R: ' + sysReq + '  \nU-S: ' + us_dashed
+        if us_nospace_inverse:
+            sysReq = 'US:' + us_nospace_inverse + '\nSR:' + sysReq
+        if us_joined_inverse:
+            sysReq = 'US: ' + us_joined_inverse + '\nSR: ' + sysReq
+        if us_joined_inverse_nofirstdots:
+            sysReq = 'US ' + us_joined_inverse_nofirstdots + '\nSR: ' + sysReq
+        if us_noletters:
+            sysReq = sysReq + ' / ' + us_noletters
+    else:
+        if sr_short:
+            sysReq = 'SR ' + sr_short
+        elif us:
+            sysReq = 'User Story: ' + us
+        elif bugfix:
+                sysReq = 'Bug Fix- ' + bugfix
+        elif bug:
+            sysReq = 'Bug' + bug + 'Test'
+        elif us_short:
+            sysReq = 'US ' + us_short
+        elif us_short_dotted:
+            sysReq = 'US: ' + us_short_dotted
+        elif us_EMPTY:
+            sysReq = 'SR: <EMPTY>\nUS: ' + us_EMPTY
+        else:
+           sysReq = 'N/A'
     testCasesList = OldCase.objects.filter(system_requirements = sysReq)
+    if check:
+        if not testCasesList:
+            sysReq = sysReq + ' '
+            testCasesList = OldCase.objects.filter(system_requirements=sysReq)
     context = RequestContext(request, {'testCasesList': testCasesList})
     return TemplateResponse(request, 'oldcat/list_cases.html', context)
 
